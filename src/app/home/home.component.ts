f4676e342507cb1e87d3274c9516f6c37f787e78
Sudheer <kumarsudheer4894@gmail.com>
2024-02-09 20:45:34 +0530

    Searching Feature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../data-type';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  popularProducts:undefined|Product[];
+  trendyProducts:undefined|Product[];
+
+  constructor(private product: ProductService){}
+
+  ngOnInit(): void{
+    this.product.popularProduct().subscribe((data)=>{
+      // console.log("Popular Data:",data);
+      this.popularProducts = data;
+    });
+    this.product.trendyProducts().subscribe((data)=>{
+      this.trendyProducts = data;
+    });
+  }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -28,4 +28,16 @@ export class ProductService {
   updateProduct(data: Product){
     return this.http.put<Product>(`http://localhost:3000/products/${data.id}`, data)
   }
+
+  popularProduct(){
+    return this.http.get<Product[]>("http://localhost:3000/products?_limit=3");
+  }
+
+  trendyProducts(){
+    return this.http.get<Product[]>("http://localhost:3000/products?_limit=10");
+  }
+
+  searchProduct(query:string){
+    return this.http.get<Product[]>(`http://localhost:3000/products?name=${query}`);
+  }
 }
