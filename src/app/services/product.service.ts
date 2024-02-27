fa978d80209cf8ecb7b697b311e9a2e1f18a8d4d
Sudheer <kumarsudheer4894@gmail.com>
2024-02-27 06:32:17 +0530

    Remove to Cart

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Product } from '../data-type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
+  cartData = new EventEmitter<Product[]>()
 
   constructor(private http: HttpClient) { }
 
@@ -50,6 +52,17 @@ export class ProductService {
       cartData = JSON.parse(localCart);
       cartData.push(data);
       localStorage.setItem('localCart', JSON.stringify(cartData));
+      this.cartData.emit(cartData);
+    }
+  }
+
+  removeItemFromCart(productId: string){
+    let cartData = localStorage.getItem('localCart');
+    if(cartData){
+      let items:Product[] = JSON.parse(cartData);
+      items = items.filter((item: Product)=>productId !== item.id);
+      localStorage.setItem('localCart', JSON.stringify(items));
+      this.cartData.emit(items);
     }
   }
 }
